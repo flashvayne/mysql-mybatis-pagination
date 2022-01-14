f797f0ee2365d749d8d66885078e97ab23de03bd
flashvayne <flashvayne@gmail.com>
2022-01-14 16:26:45 +0800

    lombok简化

--- src/main/java/com/github/flashvayne/dto/PageInfo.java
+++ src/main/java/com/github/flashvayne/dto/PageInfo.java
@@ -1,56 +1,23 @@
 package com.github.flashvayne.dto;
 
+import lombok.Data;
+
 /**
  * 分页的数据封装
  *
  * @author flashvayne
  */
+@Data
 public class PageInfo {
-    private int start = 0;
-    private int size = 10;
+    //页码
+    private int page;
 
+    //每页记录数
+    private int size;
+
+    //总记录数
     private Long total;
+
+    //当页列表
     private Object list;
-
-    public int getStart() {
-        return start;
-    }
-
-    public void setStart(int start) {
-        this.start = start;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public Long getTotal() {
-        return total;
-    }
-
-    public void setTotal(Long total) {
-        this.total = total;
-    }
-
-    public Object getList() {
-        return list;
-    }
-
-    public void setList(Object list) {
-        this.list = list;
-    }
-
-    @Override
-    public String toString() {
-        return "PageInfo{" +
-                "start=" + start +
-                ", size=" + size +
-                ", total=" + total +
-                ", list=" + list +
-                '}';
-    }
 }
